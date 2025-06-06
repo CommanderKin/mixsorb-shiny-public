2977956253dff0ddaa01d05b2a025c8fabea7721
Andrey Dyachenko <Andrey.dyachenko@skytree.com>
2025-06-06 16:56:32 +0200

    added ui elements and logic

--- server.py
+++ server.py
@@ -1,13 +1,17 @@
 from shiny import render, reactive
 
 import pandas as pd
+import numpy as np
 import db_access as dba
 
 def server(input, output, session):
     print("Server started")
+
+    # global variables 
     status_msg = reactive.Value("Waiting for input...")
     kusto_client = reactive.Value()
     experiments_list_df = reactive.Value(pd.DataFrame([]))
+    list_to_process = reactive.Value([])
 
     @reactive.Effect
     @reactive.event(input.db_connect_button)
@@ -19,6 +23,7 @@ def server(input, output, session):
             assert isinstance(client,dba.KustoClient),  "Client creation failed"
             kusto_client.set(client)
             status_msg.set("DB connect successful")
+            session.send_input_message("run_query_button", {"disabled": False}) # enable search button
         except Exception as e:
             print(f"DB connection failed: {e}")
             status_msg.set(f"Error: {e}")
@@ -29,17 +34,26 @@ def server(input, output, session):
         try: 
             status_msg.set("Running query")
             print("Running query")
-            query_text = "| summarize ts = take_any(fileCreatedUtc) by fileName | sort by ts desc | limit " + str(input.n_exp_field())
+            query_text = build_query()
             client = kusto_client()
             response_df = dba.run_query(client, query_text)
             assert isinstance(response_df, pd.DataFrame), "Something went wrong, query data is not a DF"
+            response_df = clean_response_df(response_df)
+            assert isinstance(response_df, pd.DataFrame), "Something went wrong, query data is not a DF"
             experiments_list_df.set(response_df)
+            # update the dorop-down menu
+            exp_list =[str(x) for x in response_df.iloc[:,0]]
+            print(exp_list)
+            session.send_input_message("select_exp",{ 
+                "choices": [{"label":x, "value": x} for x in exp_list], ### DEBUG THIS: https://shiny.posit.co/py/api/core/ui.input_select.html
+                "value": exp_list[0]
+            })
+            session.send_input_message("process_button", {"disabled": False})
             print(experiments_list_df())
             status_msg.set(f"Query completed")
         except Exception as e:
             print(f"Query failed: {e}")
             status_msg.set(f"Query failed: {e}")
-
 
     @output
     @render.text
@@ -49,4 +63,29 @@ def server(input, output, session):
     @output
     @render.data_frame
     def exp_list_table():
-        return render.DataGrid(experiments_list_df(), width = 800)
+        return render.DataGrid(experiments_list_df(), width = 1000)
+    
+    def build_query():
+        if input.show_seq_checkbox():
+            query_text = "| where fileName contains '" + str(input.search_field()) + \
+                "' | summarize ts = max(fileCreatedUtc) by fileName | sort by ts desc | limit " + str(np.clip(input.n_exp_field(),1,30))
+        else:
+            query_text = '| extend baseFileName = extract(@"^(.*?)(?:_\d{3})?\.xml$", 1, fileName)' + \
+            '| where baseFileName contains "' + str(input.search_field()) + '" | summarize ts = max(fileCreatedUtc), fileName = arg_max(fileCreatedUtc, fileName) by baseFileName' + \
+            '| sort by ts desc | limit ' + str(np.clip(input.n_exp_field(),1,30))
+        print(query_text)
+        return query_text
+    
+    def clean_response_df(df:pd.DataFrame):
+        try:
+            if input.show_seq_checkbox():
+                df.columns = ["File name", "Date/time created"]
+            else:
+                df.drop(columns = ['fileName'],inplace=True)
+                df.columns = ["Experiment name","Date/time created","Sequence finlename??"]
+            return df
+        except Exception as e:
+            print(f"Error cleaning df: {e}")
+
+
+
--- ui_main.py
+++ ui_main.py
@@ -1,10 +1,33 @@
 from shiny import ui
 
 app_ui = ui.page_fluid(
-    ui.h2("Connect To DB"),
-    ui.input_action_button("db_connect_button", "Connect to DB"),
-    ui.output_text_verbatim("status"),
-    ui.input_numeric("n_exp_field","N last experiments",10,min = 1, max = 30),
-    ui.input_action_button("run_query_button", "Show N last"),
-    ui.output_data_frame("exp_list_table")
+    ui.navset_tab(
+        ui.nav_panel(
+            "Main",
+            ui.h3("Get list of experiments from the database"),
+            ui.card(     
+                ui.row(
+                    ui.column(2, ui.input_action_button("db_connect_button", "Connect to DB", class_="btn btn-primary")),
+                    ui.column(2, ui.div("Status: ", class_="text-end")),
+                    ui.column(4, ui.output_text_verbatim("status"))
+                    ),
+            ),
+            ui.card(
+                ui.row(
+                    ui.column(1, ui.input_numeric("n_exp_field","N exp:",10, min = 1, max = 30)),
+                    ui.column(3, ui.input_text("search_field","Finelname contains")),
+                    ui.column(2, ui.input_checkbox("show_seq_checkbox","Show individual sequences?")),
+                    ui.column(1, ui.input_action_button("run_query_button", "Search", class_="btn btn-primary", disabled=True))
+                    )
+            ),
+            ui.row(
+                ui.column(3,ui.input_select("select_exp","Choose an experiment to process",choices=["Waiting..."])),
+                ui.column(1, ui.input_action_button("process_button", "Process", class_="btn btn-primary", disabled=True))
+            ),
+            ui.card(ui.output_data_frame("exp_list_table")),
+        ),
+        ui.nav_panel("Results", 
+                    "Results will be here"
+        )
+    )
 )
