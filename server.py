5280d8d44956ccf1dd16a80abd4f8f031d0e1b85
Andrey Dyachenko <Andrey.dyachenko@skytree.com>
2025-06-10 13:37:13 +0200

    added logics for adding new tabs

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from shiny import render, reactive
+from shiny import render, reactive, ui, module
+from ui_main import results_panel
 
 import pandas as pd
 import numpy as np
@@ -8,11 +9,12 @@ def server(input, output, session):
     print("Server started")
 
     # global variables 
-    status_msg = reactive.Value("Waiting for input...")
-    kusto_client = reactive.Value()
-    experiments_list_df = reactive.Value(pd.DataFrame([]))
-    list_to_process = reactive.Value([])
+    status_msg = reactive.Value("Waiting for input...") # status message 
+    kusto_client = reactive.Value() # reactive value to store databese client
+    experiments_list_df = reactive.Value(pd.DataFrame([])) # data frame with the list of experiments pulled from the DB
+    results_tabs = reactive.Value([]) # reacive value that stores all the currently present tabs with exp results
 
+    #Presss CONNECT TO DB button
     @reactive.Effect
     @reactive.event(input.db_connect_button)
     def on_connect_to_db():
@@ -28,6 +30,7 @@ def server(input, output, session):
             print(f"DB connection failed: {e}")
             status_msg.set(f"Error: {e}")
 
+    # Press SEARCH button
     @reactive.Effect
     @reactive.event(input.run_query_button)
     def on_run_query():
@@ -41,19 +44,34 @@ def server(input, output, session):
             response_df = clean_response_df(response_df)
             assert isinstance(response_df, pd.DataFrame), "Something went wrong, query data is not a DF"
             experiments_list_df.set(response_df)
-            # update the dorop-down menu
+            # update the drop-down menu
             exp_list =[str(x) for x in response_df.iloc[:,0]]
-            print(exp_list)
-            session.send_input_message("select_exp",{ 
-                "choices": [{"label":x, "value": x} for x in exp_list], ### DEBUG THIS: https://shiny.posit.co/py/api/core/ui.input_select.html
-                "value": exp_list[0]
-            })
+            ui.update_select(
+                "select_exp",
+                label = "Choose an experiment to process",
+                choices = exp_list,
+                selected = exp_list[0]
+            )
             session.send_input_message("process_button", {"disabled": False})
             print(experiments_list_df())
             status_msg.set(f"Query completed")
         except Exception as e:
             print(f"Query failed: {e}")
             status_msg.set(f"Query failed: {e}")
+
+    # Press PROCESS button
+    @reactive.Effect
+    @reactive.event(input.process_button)
+    def on_process():
+        try:
+            new_tab_id = "results_tab_"+ str(len(results_tabs.get()) + 1)
+            new_tab_number = new_tab_id,len(results_tabs.get()) + 1
+            new_tab_content = input.select_exp()
+            updated_tabs = results_tabs.get() + [results_panel(new_tab_id,new_tab_number,new_tab_content)]
+            results_tabs.set(updated_tabs)
+            reset_reactive_elements()
+        except Exception as e:
+            status_msg.set(f"Tab creation failed: {e}")
 
     @output
     @render.text
@@ -87,5 +105,54 @@ def server(input, output, session):
         except Exception as e:
             print(f"Error cleaning df: {e}")
 
+    @output
+    @render.ui
+    def main_ui():
+        return ui.navset_tab(
+            ui.nav_panel(
+                "Main",
+                ui.h3("Get list of experiments from the database"),
+                ui.card(     
+                    ui.row(
+                        ui.column(2, ui.input_action_button("db_connect_button", "Connect to DB", class_="btn btn-primary")),
+                        ui.column(2, ui.div("Status: ", class_="text-end")),
+                        ui.column(4, ui.output_text_verbatim("status"))
+                        ),
+                ),
+                ui.card(
+                    ui.row(
+                        ui.column(1, ui.input_numeric("n_exp_field","N exp:",10, min = 1, max = 30)),
+                        ui.column(3, ui.input_text("search_field","Finelname contains")),
+                        ui.column(2, ui.input_checkbox("show_seq_checkbox","Show individual sequences?")),
+                        ui.column(1, ui.input_action_button("run_query_button", "Search", class_="btn btn-primary", disabled=True))
+                        )
+                ),
+                ui.row(
+                    ui.column(3,ui.input_select("select_exp","Choose an experiment to process",choices=["Waiting..."])),
+                    ui.column(1, ui.input_action_button("process_button", "Process", class_="btn btn-primary", disabled=True))
+                ),
+                ui.card(ui.output_data_frame("exp_list_table")),
+            ),
+            *[panel for panel in results_tabs.get()],
+            id = "main_ui_id"
+        )
+    
+    # Reset the ui elements after redrawing the ui upon adding a new tab
+    def reset_reactive_elements():
+        session.send_input_message("process_button", {"disabled": False})
+        session.send_input_message("run_query_button", {"disabled": False})
+        exp_list =[str(x) for x in experiments_list_df.get().iloc[:,0]]
+        ui.update_select(
+            "select_exp",
+            label = "Choose an experiment to process",
+            choices = exp_list,
+            selected = exp_list[0]
+        )
 
+@module.server
+def row_server(input, output, session):
+    @output
+    @render.text
+    def text_out():
+        pass
 
--- ui_main.py
+++ ui_main.py
@@ -1,33 +1,14 @@
-from shiny import ui
+from shiny import ui, module
 
 app_ui = ui.page_fluid(
-    ui.navset_tab(
-        ui.nav_panel(
-            "Main",
-            ui.h3("Get list of experiments from the database"),
-            ui.card(     
-                ui.row(
-                    ui.column(2, ui.input_action_button("db_connect_button", "Connect to DB", class_="btn btn-primary")),
-                    ui.column(2, ui.div("Status: ", class_="text-end")),
-                    ui.column(4, ui.output_text_verbatim("status"))
-                    ),
-            ),
-            ui.card(
-                ui.row(
-                    ui.column(1, ui.input_numeric("n_exp_field","N exp:",10, min = 1, max = 30)),
-                    ui.column(3, ui.input_text("search_field","Finelname contains")),
-                    ui.column(2, ui.input_checkbox("show_seq_checkbox","Show individual sequences?")),
-                    ui.column(1, ui.input_action_button("run_query_button", "Search", class_="btn btn-primary", disabled=True))
-                    )
-            ),
-            ui.row(
-                ui.column(3,ui.input_select("select_exp","Choose an experiment to process",choices=["Waiting..."])),
-                ui.column(1, ui.input_action_button("process_button", "Process", class_="btn btn-primary", disabled=True))
-            ),
-            ui.card(ui.output_data_frame("exp_list_table")),
-        ),
-        ui.nav_panel("Results", 
-                    "Results will be here"
-        )
-    )
+    ui.output_ui("main_ui")
 )
+
+
+@module.ui
+def results_panel(panel_number,exp_name):
+    tab_title = f"New tab {panel_number}"
+    return ui.nav_panel(
+        tab_title,
+        ui.p(f"We will have the processing results of the {exp_name} here soon"),
+    )
